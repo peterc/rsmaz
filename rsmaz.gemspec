05686de1b10c8671c99234d6c8559153093bb2d1
Santosh Mani <santosh.mani@abletech.co.nz>
2023-02-22 16:34:59 +1300

    - Change Fixnum reference to Integer - Update Gemspec - Add.ruby-version - Fix minitest deprecations from test - Update Readme.md

--- rsmaz.gemspec
+++ rsmaz.gemspec
@@ -2,20 +2,21 @@
 
 Gem::Specification.new do |s|
   s.name = %q{rsmaz}
-  s.version = "0.0.4"
+  s.version = "0.0.5"
 
-  s.authors = ["Peter Cooper"]
-  s.date = '2011-03-26'
+  s.authors = ["Peter Cooper, Abletech"]
+  s.date = '2023-02-22'
   s.description = %q{Short String Compression for Ruby. RSmaz is a pure-Ruby port of the Smaz short string compression algorithm by Salvatore Sanfilippo and released as a C library at: http://github.com/antirez/smaz/tree/master  I've done some initial cleanup of a pure Ruby->C port, but this is not yet complete. It does pass the specs, however! Feel free to clean it up as it's a bit memory inefficient right now... :)}
-  s.email = ["git@peterc.org"]
+  s.email = ["santosh.mani@abletech.co.nz"]
   s.extra_rdoc_files = ["HISTORY", "README.rdoc"]
   s.files = [ 'HISTORY',
               'Rakefile',
               'README.rdoc',
+              '.ruby-version',
               'test/test_helper.rb',
               'test/test_rsmaz.rb',
               'lib/rsmaz.rb']
-  s.homepage = 'http://github.com/peterc/rsmaz'
+  s.homepage = 'https://github.com/abletech/rsmaz'
   s.require_paths = ["lib"]
   s.summary = %q{Short String Compression for Ruby}
   s.add_development_dependency('minitest')
--- test/test_rsmaz.rb
+++ test/test_rsmaz.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
 
 describe RSmaz do
-  
+
   #before(:each) do
   #  # Do some memory leak checking
   #  puts "\nMemory used: #{memory_usage}K"
@@ -12,11 +12,11 @@ describe RSmaz do
   #end
   
   it "should compress 'the' to one byte" do
-    RSmaz.compress("the").length.must_equal 1
+    _(RSmaz.compress("the").length).must_equal 1
   end
   
   it "should compress 'thex' to two bytes" do
-    RSmaz.compress("thex").length.must_equal 2
+    _(RSmaz.compress("thex").length).must_equal 2
   end
   
   it "should compress and decompress strings back to the same string" do
@@ -26,29 +26,32 @@ describe RSmaz do
       "this is a test",
       "Let's try a SlIgHtLy Funkie938R str1ng!?!?x",
       "What about a long one?" * 100
-    ].each { |str| RSmaz.decompress(RSmaz.compress(str)).must_equal str }    
+    ].each do |str|
+      result = RSmaz.decompress(RSmaz.compress(str))
+      _(result).must_equal str
+    end
   end
   
   it "should properly decode a reference compression (so the internal coding doesn't change)" do
-    RSmaz.decompress("\020\230`A\376o\f\026\030").must_equal "hello world"
+    _(RSmaz.decompress("\020\230`A\376o\f\026\030")).must_equal "hello world"
   end
   
   it "should compress to the same extent as the reference smaz implementation" do
-    RSmaz.compress("foobar").length.must_equal 4
-    RSmaz.compress("Nel mezzo del cammin di nostra vita, mi ritrovai in una selva oscura").length.must_equal 46
+    _(RSmaz.compress("foobar").length).must_equal 4
+    _(RSmaz.compress("Nel mezzo del cammin di nostra vita, mi ritrovai in una selva oscura").length).must_equal 46
   end
   
   it "should compress and decompress lots of random strings without issues" do
     100.times do
       str = (1..100).map { |a| (rand(26)+97).chr }.join
-      RSmaz.decompress(RSmaz.compress(str)).length.must_equal str.length
+      _(RSmaz.decompress(RSmaz.compress(str)).length).must_equal str.length
     end
   end
 
   it "should compress and decompress lots of random strings without issues (again)" do
     100.times do
       str = (1..100).map { |a| (rand(26)+97).chr }.join
-      RSmaz.decompress(RSmaz.compress(str)).length.must_equal str.length
+      _(RSmaz.decompress(RSmaz.compress(str)).length).must_equal str.length
     end
   end
   
@@ -56,15 +59,15 @@ describe RSmaz do
     # U+2603 = Unicode snowman
     
     it "should be able to compress strings with arbitrary encodings" do
-      RSmaz.decompress(RSmaz.compress("\u2603")).force_encoding('utf-8').must_equal "\u2603"
+      _(RSmaz.decompress(RSmaz.compress("\u2603")).force_encoding('utf-8')).must_equal "\u2603"
     end
     
     it "should return binary strings upon compressing" do
-      RSmaz.compress("\u2603").encoding.must_equal Encoding.find('binary')
+      _(RSmaz.compress("\u2603").encoding).must_equal Encoding.find('binary')
     end
     
     it "should return binary strings upon decompressing" do
-      RSmaz.decompress(RSmaz.compress("\u2603")).encoding.must_equal Encoding.find('binary')
+      _(RSmaz.decompress(RSmaz.compress("\u2603")).encoding).must_equal Encoding.find('binary')
     end
   end
 end
